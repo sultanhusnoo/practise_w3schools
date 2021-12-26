dd73c406d13dafc8e7e000a156c2433f03af0114
sultan <sultan.husnoo@gmail.com>
2021-12-26 13:38:02 +0400

    one more commit

--- practise_javascript/script.js
+++ practise_javascript/script.js
@@ -1,4 +1,26 @@
 
+function turnBlue(){
+    document.getElementById('myBody').style.backgroundColor = 'LightBlue'
+}
+
+function darkMode(){
+    document.getElementById('myBody').style.backgroundColor = "#181818";
+    var myMultipleTexts = document.getElementsByName('mt');
+
+    for (var i = 0, max = myMultipleTexts.length; i < max; i++) {
+        myMultipleTexts[i].style.color = 'white';
+    }
+}
+
+function lightMode(){
+    document.getElementById('myBody').style.backgroundColor = 'white';
+    var myMultipleTexts = document.getElementsByName('mt');
+
+    for (var i = 0, max = myMultipleTexts.length; i < max; i++) {
+        myMultipleTexts[i].style.color = 'black';
+    }
+}
+
 function bulbON(){
     document.getElementById('myBulb').src = 'pic_bulbon.gif';
 }
@@ -6,13 +28,3 @@ function bulbON(){
 function bulbOFF(){
     document.getElementById('myBulb').src = 'pic_bulboff.gif';
 }
-
-function darkMode(){
-    document.getElementById('myBody').style.backgroundColor = "#181818";
-    document.getElementsByClassName('bodyText').style.color = 'white';
-}
-
-function lightMode(){
-    document.getElementById('myBody').style.backgroundColor = 'white';
-    document.getElementById('bodyText').style.color = 'black';
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
