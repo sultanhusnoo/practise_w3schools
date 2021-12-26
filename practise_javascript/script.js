54e67df0ae07ba208a9db9b3355de716c7e4c47f
sultan <sultan.husnoo@gmail.com>
2021-12-26 11:36:07 +0400

    second commit

---
+++ practise_javascript/script.js
@@ -0,0 +1,18 @@
+
+function bulbON(){
+    document.getElementById('myBulb').src = 'pic_bulbon.gif';
+}
+
+function bulbOFF(){
+    document.getElementById('myBulb').src = 'pic_bulboff.gif';
+}
+
+function darkMode(){
+    document.getElementById('myBody').style.backgroundColor = "#181818";
+    document.getElementsByClassName('bodyText').style.color = 'white';
+}
+
+function lightMode(){
+    document.getElementById('myBody').style.backgroundColor = 'white';
+    document.getElementById('bodyText').style.color = 'black';
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
